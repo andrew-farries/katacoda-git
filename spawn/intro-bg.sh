8d4fbfe63dd8d32e41ae7603a161ffae1e4dd3a9
Andrew Farries <andyrb@gmail.com>
2021-03-25 12:34:10 +0000

    Just use a foreground script

--- spawn/intro-bg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull redgatefoundry/spawnctl:0.20
-
-# Create a file to indicate to the foreground script that the background task has completed.
-touch /opt/.intro-bg-complete
---
+++ spawn/intro.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker pull redgatefoundry/spawnctl:0.20
