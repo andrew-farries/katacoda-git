d4f5f5a8889810893badff8988d2287632097e3b
Andrew Farries <andyrb@gmail.com>
2021-04-08 11:24:44 +0100

    remove

--- spawn/intro.sh
+++ spawn/intro.sh
@@ -4,7 +4,7 @@ ctltag="no-certs"
 
 docker pull surjection/spawnctl:$ctltag
 
-alias spawnctl='docker run --rm -e SPAWNCTL_API_ENDPOINT -e SPAWNCTL_ACCESS_TOKEN surjection/spawnctl:$ctltag'
+alias spawnctl='docker run --rm surjection/spawnctl:$ctltag'
 
 export SPAWNCTL_API_ENDPOINT='katacoda.api.staging.spawn.cc'
 export SPAWNCTL_ACCESS_TOKEN='eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2ZTE5NzhjODkxMTA1NTg1ZjExYjU4YTcyZGIyZWI5YiIsImVtYWlsIjoic3Bhd24ucmVkZ2F0ZStrYXRhY29kYUBnbWFpbC5jb20iLCJqdGkiOiI3MGUwNDJjZjNjYjE0ZGQ4YWQ5YzkxYjY2ZTA4MDU5NSIsImlzcyI6IlNwYXduQXBpU2VydmVyIiwiYXVkIjoiU3Bhd25BcGlTZXJ2ZXIifQ.cviLJVBS119M8KGUFszO1jGaj9E5wfM0Gh0rh7fVYkw'
